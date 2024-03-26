0a616c17fe939616b6b2f96cc42dae79dae35b15
teralin <ty2001x@gmail.com>
2024-03-26 20:11:11 +0800

    good with fetchcontent

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include <htmlcxx/html/ParserDom.h>
 extern "C"
 {
-	#include <lua/lua.h>
-	#include <lua/lualib.h>
-	#include <lua/lauxlib.h>
+	#include <lua.h>
+	#include <lualib.h>
+	#include <lauxlib.h>
 }
 
 std::string html;
